7a4650f391f7866fc1e1529661ea4f1a8f2c0970
Reherc <amaury.vanoorenberghe@yahoo.com>
2020-08-01 01:19:14 +0200

    Updated GSL version

---
+++ Distance.TextureModifier/Mod.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Reactor.API.Attributes;
+using Reactor.API.Interfaces.Systems;
+using UnityEngine;
+
+namespace ExampleNamespace
+{
+    [ModEntryPoint(ModID)]
+    public class Mod : MonoBehaviour
+    {
+        public const string ModID = "com.example.someone/ExampleModID";
+
+        public void Initialize(IManager manager)
+        {
+            // This will run after Awake(), unless 
+            // specified otherwise in ModEntryPoint
+            // attribute property AwakeAfterInitialize.
+        }
+
+        public void Awake()
+        {
+
+        }
+    }
+}
