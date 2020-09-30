61fd3f9e59fc6607bc4925d2e1b37f1fbf5df36a
Reherc <amaury.vanoorenberghe@yahoo.com>
2020-09-30 22:07:00 +0200

    Re added file to source control

---
+++ Distance.CustomWheelHologram/Harmony/Assembly-CSharp/WheelPOV/Awake.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Distance.CustomWheelHologram.Harmony
+{
+    [HarmonyPatch(typeof(WheelPOV), "Awake")]
+    internal class WheelPOV__Awake
+    {
+        [HarmonyPostfix]
+        internal static void Postfix(WheelPOV __instance)
+        {
+            if (!Mod.Instance.OriginalImage)
+            {
+                Mod.Instance.OriginalImage = __instance.renderer_.material.mainTexture as Texture2D;
+            }
+        }
+    }
+}
