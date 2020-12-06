088111e5a1b8ead699cfac00fa898317a891737f
Reherc <amaury.vanoorenberghe@yahoo.com>
2020-12-06 01:45:44 +0100

    Added icons for tools

---
+++ App.Icons/Resources.cs
@@ -0,0 +1,11 @@
+﻿using Eto.Drawing;
+
+public static class Resources
+{
+	public static string ResourcesRoot => typeof(Resources).Assembly.GetName().Name;
+
+	public static Icon GetIcon(string iconName)
+	{
+		return Icon.FromResource($"{ResourcesRoot}.Icons.{iconName}");
+	}
+}
