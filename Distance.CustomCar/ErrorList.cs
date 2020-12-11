d4716e26b8a2ddb0a0ae38aec2d4324fee6311ef
Reherc <amaury.vanoorenberghe@yahoo.com>
2020-12-11 17:41:53 +0100

    Made ErrorList directly inherit List<string>

--- Distance.CustomCar/ErrorList.cs
+++ Distance.CustomCar/ErrorList.cs
@@ -1,9 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using Centrifuge.Distance.Data;
+using Centrifuge.Distance.Game;
+using System.Collections.Generic;
 
 namespace Distance.CustomCar
 {
 	public class ErrorList : List<string>
 	{
+		public new void Add(string val)
+		{
+			base.Add(val);
+		}
+
 		public override string ToString()
 		{
 			return string.Join("\n", ToArray());
@@ -11,7 +18,9 @@ namespace Distance.CustomCar
 
 		public void Show()
 		{
-			MessageBox.
+			MessageBox.Create($"An error occured while loading the cars\n{ToString()}", "Custom car - Errors")
+			.SetButtons(MessageButtons.Ok)
+			.Show();
 		}
 	}
 }
