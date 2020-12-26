858fc86fa3d7d7c4dfad6c53732b6b7ef1fc9a3c
Reherc <amaury.vanoorenberghe@yahoo.com>
2020-12-26 19:13:50 +0100

    Added hud stats in level editor

---
+++ Distance.NitronicHUD/Harmony/Assembly-CSharp/StatsManager/OnEventPlayerAdded.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Distance.NitronicHUD.Harmony
+{
+	[HarmonyPatch(typeof(StatsManager), "OnEventPlayerAdded")]
+	internal static class StatsManager__OnEventPlayerAdded
+	{
+		[HarmonyTranspiler]
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instr)
+		{
+			return instr.Skip(5); // Skip the default check "if is in level editor, don't create player stats object"
+		}
+	}
+}
