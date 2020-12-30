0e75b06d3346127993f04557c3f2e9af84291ffb
Reherc <amaury.vanoorenberghe@yahoo.com>
2020-12-30 03:46:28 +0100

    Moved the adventuremaker common code into a shared project

--- publish.cmd
+++ publish.cmd
@@ -2,10 +2,16 @@
 PUSHD %~dp0
 
 REM ===== COMPILE =====
+REM Adventure Maker App
+PUSHD App.AdventureMaker.Windows
+dotnet publish -p:PublishProfile=Properties\PublishProfiles\win-x86.pubxml -o:publish\win-x86
+POPD
+PUSHD App.AdventureMaker.Linux
+dotnet publish -p:PublishProfile=Properties\PublishProfiles\linux-x64.pubxml -o:publish\linux-x64
+POPD
 REM Custom Death Messages App
 PUSHD App.CustomDeathMessages.Windows
 dotnet publish -p:PublishProfile=Properties\PublishProfiles\win-x86.pubxml -o:publish\win-x86
-dotnet publish -p:PublishProfile=Properties\PublishProfiles\win-x64.pubxml -o:publish\win-x64
 POPD
 PUSHD App.CustomDeathMessages.Linux
 dotnet publish -p:PublishProfile=Properties\PublishProfiles\linux-x64.pubxml -o:publish\linux-x64
@@ -15,13 +21,20 @@ REM ===== BUNDLE =====
 SET PACKAGES=%CD%\Build
 CALL :MAKEDIR %PACKAGES%
 
+REM Adventure Maker App
+SET APPNAME=Distance Adventure Maker - Editor
+SET TARGET=%PACKAGES%\%APPNAME%
+
+CALL :MAKEDIR %PACKAGES%\%APPNAME%
+CALL :COPYDIR "%CD%\App.AdventureMaker.Windows\publish\win-x86" "%TARGET%\win-x86"
+CALL :COPYDIR "%CD%\App.AdventureMaker.Linux\publish\linux-x64" "%TARGET%\linux-x64"
+
 REM Custom Death Messages App
 SET APPNAME=Distance Custom Death Messages - Editor
 SET TARGET=%PACKAGES%\%APPNAME%
 
 CALL :MAKEDIR %PACKAGES%\%APPNAME%
 CALL :COPYDIR "%CD%\App.CustomDeathMessages.Windows\publish\win-x86" "%TARGET%\win-x86"
-CALL :COPYDIR "%CD%\App.CustomDeathMessages.Windows\publish\win-x64" "%TARGET%\win-x64"
 CALL :COPYDIR "%CD%\App.CustomDeathMessages.Linux\publish\linux-x64" "%TARGET%\linux-x64"
 
 POPD
