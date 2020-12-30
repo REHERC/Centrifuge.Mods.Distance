97d4417a17c2248a805a85eace7553b91251f3e1
Reherc <amaury.vanoorenberghe@yahoo.com>
2020-12-30 03:56:53 +0100

    Added entry points, set app icons and set build scripts

---
+++ App.AdventureMaker.Linux/Program.cs
@@ -0,0 +1,24 @@
+﻿using Eto;
+using Eto.Forms;
+using System;
+using System.Globalization;
+using System.Threading;
+
+namespace App.AdventureMaker.Linux
+{
+	internal class Program
+	{
+		[STAThread]
+		private static void Main()
+		{
+			CultureInfo english = new CultureInfo("en-US");
+			Thread.CurrentThread.CurrentCulture = english;
+			Thread.CurrentThread.CurrentUICulture = english;
+
+			CultureInfo.CurrentCulture = english;
+			CultureInfo.CurrentUICulture = english;
+
+			//new Application(Platforms.WinForms).Run(new MainWindow());
+		}
+	}
+}
