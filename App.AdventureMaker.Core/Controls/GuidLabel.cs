b3bd45aa2a299abc66cbf03749c9e26c2f818c5b
Reherc <amaury.vanoorenberghe@yahoo.com>
2021-01-05 20:42:32 +0100

    Made punctuation ciastex-compliant

--- App.AdventureMaker.Core/Controls/GuidLabel.cs
+++ App.AdventureMaker.Core/Controls/GuidLabel.cs
@@ -6,7 +6,7 @@ namespace App.AdventureMaker.Core.Controls
 	public class GuidLabel : ControlWithButtonBase<Label>
 	{
 		public const string MESSAGE_TITLE = "Regenerate GUID";
-		public const string MESSAGE_TEXT = "Are you sure you want to generate a new identifier ?\nThis identifier is used for linking save data, generating a new one will make old saves incompatible !";
+		public const string MESSAGE_TEXT = "Are you sure you want to generate a new identifier?\nThis identifier is used for linking save data, generating a new one will make old saves incompatible!";
 
 		public string Text
 		{
