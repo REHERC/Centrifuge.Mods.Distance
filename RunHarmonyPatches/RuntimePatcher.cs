f8ea7b22c36660436f50861d47c5ce4fbf780da7
Reherc <amaury.vanoorenberghe@yahoo.com>
2021-01-09 01:30:05 +0100

    Centrifuge's RuntimePAtcher.AutoPatch for third party apps

---
+++ RunHarmonyPatches/RuntimePatcher.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using System.Reflection;
+
+public static class RuntimePatcher
+{
+	public static readonly Harmony HarmonyInstance;
+
+	static RuntimePatcher()
+	{
+		HarmonyInstance = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), Assembly.GetExecutingAssembly().GetName().FullName);
+	}
+
+	public static void AutoPatch()
+	{
+		HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+	}
+}
