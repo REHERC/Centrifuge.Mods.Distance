518ab718e5fb45c1dd53af85e9f1b2c4941ab3e6
Reherc <amaury.vanoorenberghe@yahoo.com>
2021-02-01 00:37:33 +0100

    Removed separator between some properties because it wasn't 1px tall on linux builds

--- App.AdventureMaker.Core/Views/Pages/LevelSets/LevelSetsPropertiesView.cs
+++ App.AdventureMaker.Core/Views/Pages/LevelSets/LevelSetsPropertiesView.cs
@@ -36,12 +36,12 @@ namespace App.AdventureMaker.Core.Views
 			properties.AddRow("Description", descriptionBox = new TextBox());
 			properties.AddRow("Icon", iconBox = new TextBoxWithButton());
 
-			properties.Separator();
+			//properties.Separator();
 
 			properties.AddRow("Campaign playlist", campaignDisplayBox = new BooleanSelector());
 			properties.AddRow("Sprint playlist", sprintDisplayBox = new BooleanSelector());
 
-			properties.Separator();
+			//properties.Separator();
 
 			properties.AddRow("Unique ID", guidBox = new GuidLabel());
 			//properties.AddRow("Playlist locking", new EnumDropDown<PlaylistUnlock>());
