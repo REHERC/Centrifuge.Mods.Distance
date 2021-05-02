3fa86a21051a213e2985ddf9d4a0b7cda9e00b20
Reherc <amaury.vanoorenberghe@yahoo.com>
2021-05-02 22:37:28 +0200

    Distance waiting simulator™

--- Distance.AdventureMaker/Loader/Steps/CampaignImporter.cs
+++ Distance.AdventureMaker/Loader/Steps/CampaignImporter.cs
@@ -1,5 +1,8 @@
 ﻿using Centrifuge.Distance.Game;
+using System;
 using System.Collections;
+using System.Text;
+using UnityEngine;
 using static Distance.AdventureMaker.Loader.CampaignLoaderLogic;
 
 namespace Distance.AdventureMaker.Loader.Steps
@@ -12,7 +15,30 @@ namespace Distance.AdventureMaker.Loader.Steps
 
 		public override IEnumerator Run(Task.Status status)
 		{
-			yield break;
+			DiscordRpc.RichPresence rpc = new DiscordRpc.RichPresence();
+			rpc.details = "Waiting...";
+			rpc.state = "In Main Menu";
+			rpc.startTimestamp = 0L;
+			rpc.endTimestamp = 0L;
+			rpc.largeImageKey = "official_level";
+
+			TimeSpan span;
+			StringBuilder dots;
+			double totalTime = 0;
+			while (true)
+			{
+				dots = new StringBuilder();
+				dots.Append('.', 1 + ((int)(totalTime * 2) % 3));
+
+				totalTime += Time.deltaTime;
+
+				span = TimeSpan.FromSeconds(totalTime);
+				status.SetText($"You have been waiting for {Colors.red.ToFormattedHex()}{span.Hours:000}:{span.Minutes:00}:{span.Seconds:00}[/c][-] {dots}\n{Colors.gray.ToFormattedHex()}There is nothing of interest here (really)");
+				status.SetProgress((uint)span.Milliseconds, 1000);
+
+				DiscordRpc.UpdatePresence(ref rpc);
+				yield return null;
+			}
 		}
 	}
 }
